cf8701e4fa6f6bda0e11664c0dd98e748a76e1b1
Mark <shuyumark@gmail.com>
2016-03-25 21:18:50 +0800

    basic

---
+++ src/main/java/io/ztmark/controller/HomeController.java
@@ -0,0 +1,18 @@
+package io.ztmark.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: Mark
+ * Date  : 16/3/25
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Spring Boot!";
+    }
+
+}
